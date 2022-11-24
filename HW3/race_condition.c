8d253dc341036449a5a5b1e6b95df99e0d22f032
jeffYang1224 <92380307+qwe8496516@users.noreply.github.com>
2022-11-24 23:55:12 +0800

    Update race_condition.c

--- HW3/race_condition.c
+++ HW3/race_condition.c
@@ -6,27 +6,32 @@ int data = 0;
 
 void *subchild()
 {
-	for(int i = 0; i < LOOP; i++){
+	for (int i = 0; i < LOOP; i++)
+	{
 		data--;
 	}
 }
 
 void *addchild()
 {
-	for(int i = 0; i < LOOP; i++){
+	for (int i = 0; i < LOOP; i++)
+	{
 		data++;
 	}
 }
 
 int main()
 {
-	pthread_t thread1,thread2;
-	
-	pthread_create(&thread1,NULL,subchild,NULL);
-	pthread_create(&thread2,NULL,addchild,NULL);
-		
-	pthread_join(thread1,NULL);
-	pthread_join(thread2,NULL);
-	printf("global data = %d\n",data);	
+	// generate two thread
+	pthread_t thread1, thread2;
+
+	// thread creation (ID address,Attribute,function,parameter)
+	pthread_create(&thread1, NULL, subchild, NULL);
+	pthread_create(&thread2, NULL, addchild, NULL);
+
+	// ensure the thread finish before main process finish (ID,return)
+	pthread_join(thread1, NULL);
+	pthread_join(thread2, NULL);
+	printf("global data = %d\n", data);
 	return 0;
 }
