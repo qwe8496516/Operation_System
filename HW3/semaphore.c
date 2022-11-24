5360918ae964b0724c8a5b1580498e07694bf5b3
jeffYang1224 <92380307+qwe8496516@users.noreply.github.com>
2022-11-25 00:01:05 +0800

    Update semaphore.c

--- HW3/semaphore.c
+++ HW3/semaphore.c
@@ -1,22 +1,21 @@
 #include <stdio.h>
 #include <pthread.h>
 #include <stdatomic.h>
-#define LOOP 3
+#define LOOP 100000
 
 typedef struct global_data
 {
-	int data;
-	int count;
+	int data;	// calculate
+	int count; // resource quantity
 	atomic_flag m;
 } global_data;
 global_data gdata;
 
-
 void semwait()
 {
 	while (atomic_flag_test_and_set(&gdata.m));
-	// P
-	while(gdata.count <= 0);
+	// P -> if resource is greater than 0 then allocation the resource
+	while (gdata.count <= 0);
 	gdata.count--;
 	/* --- */
 	atomic_flag_clear(&gdata.m);
@@ -24,13 +23,14 @@ void semwait()
 
 void semsignal()
 {
-	// V
+	// V - > release the resource
 	gdata.count++;
 }
 
 void *subchild()
 {
-	for(int i = 0; i < LOOP; i++){
+	for (int i = 0; i < LOOP; i++)
+	{
 		semwait();
 		gdata.data--;
 		// printf("sub = %d\n",gdata.data);
@@ -41,7 +41,8 @@ void *subchild()
 
 void *addchild()
 {
-	for(int i = 0; i < LOOP; i++){
+	for (int i = 0; i < LOOP; i++)
+	{
 		semwait();
 		gdata.data++;
 		// printf("add = %d\n",gdata.data);
@@ -52,16 +53,21 @@ void *addchild()
 
 int main()
 {
-	pthread_t thread1,thread2;
-	
+	// generate two thread
+	pthread_t thread1, thread2;
+
+	// the resource quanyity
 	gdata.count = 1;
-	pthread_create(&thread1,NULL,subchild,NULL);
-	pthread_create(&thread2,NULL,addchild,NULL);
-	
-	pthread_join(thread1,NULL);
-	pthread_join(thread2,NULL);
-	
-	printf("global data = %d,%d\n",gdata.data,gdata.count);
-	
+
+	// thread creation (ID address,Attribute,function,parameter)
+	pthread_create(&thread1, NULL, subchild, NULL);
+	pthread_create(&thread2, NULL, addchild, NULL);
+
+	// ensure the thread finish before main process finish (ID,return)
+	pthread_join(thread1, NULL);
+	pthread_join(thread2, NULL);
+
+	printf("global data = %d\n", gdata.data);
+
 	return 0;
 }
